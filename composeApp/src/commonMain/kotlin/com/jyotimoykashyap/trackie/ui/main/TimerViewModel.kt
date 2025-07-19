b8713af2168dc241bf2fa8995ee2fde2e7e8a7ac
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2025-07-19 12:26:15 +0530

    Added clean and making a change to source for triggering gh-pages workflow

--- composeApp/src/commonMain/kotlin/com/jyotimoykashyap/trackie/ui/main/TimerViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/jyotimoykashyap/trackie/ui/main/TimerViewModel.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.mutableStateOf
 import com.jyotimoykashyap.trackie.models.TimeEntry
 import com.jyotimoykashyap.trackie.repository.DefaultTimeLogRepositoryImpl
-import com.jyotimoykashyap.trackie.util.Logger
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -48,7 +47,6 @@ class TimerViewModel {
     fun loadLogEntries() {
         // Load entries and reverse them to show the newest first
         _logEntries.value = repository.readAllEntries().reversed()
-        Logger.log("All entries loaded")
     }
 
     fun toggleTimer() {
